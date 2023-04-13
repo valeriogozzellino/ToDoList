3d77b787ab39f8622387cc546e4ef7ad39888932
Valerio Gozzellino <gozzellinovalerio@gmail.com>
2023-04-14 01:34:25 +0200

    change the name of the Html file, Upload of style and html, implement js to add item in the list

---
+++ script.js
@@ -0,0 +1,12 @@
+function AddList(){
+    let lista;
+    let testo= document.getElementById("inputext").value;
+    const input=testo;
+
+    if(testo=" "){
+        lista= "inserisci un elemento da aggiungere alla lista";
+    else{
+        lista=
+    }
+    }
+}
